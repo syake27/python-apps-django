72022ed75362d699a86813e9ce0cf2e7508b8e71
syake27 <ayumu.inutsuka0427@gmail.com>
2025-10-15 12:20:12 +0900

    assignment

--- work09/urls.py
+++ work09/urls.py
@@ -6,4 +6,7 @@ app_name = "work09"
 urlpatterns = [
     path("", views.todo_list, name="todo_list"),
     path("create/", views.todo_create, name="todo_create"),
+    path("edit/<int:pk>/", views.todo_edit, name="todo_edit"),
+    path("toggle/<int:pk>/", views.todo_toggle, name="todo_toggle"),
+    path("delete/<int:pk>/", views.todo_delete, name="todo_delete")
 ]
--- work09/views.py
+++ work09/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Todo
 from .forms import TodoForm
+from datetime import date
 
 
 # Create your views here.
@@ -8,6 +9,21 @@ from .forms import TodoForm
 
 def todo_list(request):
     todos = Todo.objects.all().order_by("due_date")
+    today = date.today()
+    sort_order = request.GET.get("sort", "asc")  # パラメータ sort を取得
+    if sort_order == "desc":
+        todos = Todo.objects.all().order_by("-due_date")
+    else:
+        todos = Todo.objects.all().order_by("due_date")
+
+    next_sort = "desc" if sort_order == "asc" else "asc"
+
+    status_filter = request.GET.get("status", "all")
+
+    if status_filter == "completed":
+        todos = todos.filter(is_completed=True)
+    elif status_filter == "incomplete":
+        todos = todos.filter(is_completed=False)
 
     if request.method == "POST":
         form = TodoForm(request.POST)
@@ -17,7 +33,18 @@ def todo_list(request):
     else:
         form = TodoForm()
 
-    return render(request, "work09/todo_list.html", {"todos": todos, "form": form})
+    return render(
+        request,
+        "work09/todo_list.html",
+        {
+            "todos": todos,
+            "form": form,
+            "today": today,
+            "next_sort": next_sort,
+            "sort_order": sort_order,
+            "status_filter": status_filter,
+        },
+    )
 
 
 def todo_create(request):
@@ -31,3 +58,28 @@ def todo_create(request):
     else:
         form = TodoForm()
     return render(request, "work09/todo_create.html", {"form": form})
+
+
+def todo_edit(request, pk):
+    todo = Todo.objects.get(pk=pk)
+    if request.method == "POST":
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            form.save()
+            return redirect("work09:todo_list")
+    else:
+        form = TodoForm(instance=todo)
+    return render(request, "work09/todo_edit.html", {"form": form, "todo": todo})
+
+
+def todo_toggle(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    todo.is_completed = not todo.is_completed  # ← True/Falseを反転
+    todo.save()
+    return redirect("work09:todo_list")
+
+
+def todo_delete(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    todo.delete()
+    return redirect("work09:todo_list")
